f5709d67a99b0a5ca0349983d91f1c5be25d7eb7
Alexander Evgin <aleasims@gmail.com>
2024-03-27 13:51:49 +0400

    Rework Nix flakes for this repo
    Added building and testing to root flake.

--- nix/intx.nix
+++ nix/intx.nix
@@ -1,17 +1,15 @@
-{ pkgs, stdenv, ... }:
+{ repo, pkgs, stdenv, enable_debug, ... }:
 stdenv.mkDerivation {
   name = "intx";
 
-  src = pkgs.fetchFromGitHub {
-    owner = "chfast";
-    repo = "intx";
-    rev = "e0732242e36b3bb08cc8cc23445b2bee7c28b9d0";
-    hash = "sha256-XNpSO++FuG27Bgf5My9eJ/zrQlsWcGajfq4Y2UI2Kpk=";
-  };
+  src = repo;
 
   nativeBuildInputs = [ pkgs.cmake ];
 
   cmakeFlags = [
     "-DINTX_TESTING=OFF"
   ];
+
+  doCheck = true;
+  dontStrip = enable_debug;
 }
