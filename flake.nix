f31d0fbace2f1575d39304e1adef8f2f0180961c
Alexander Evgin <aleasims@gmail.com>
2024-04-12 15:20:46 +0400

    Add dependencies to zkEVM-framework flake
    Added evm-assigner, crypto3, blueprint and evmc.

--- flake.nix
+++ flake.nix
@@ -2,13 +2,33 @@
   description = "Nix flake for zkEVM framework";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs"; };
+    nixpkgs = { url = "github:NixOS/nixpkgs/nixos-23.11"; };
+    # TODO: remove ssh type here after evm-assigner goes public
+    nil_evm_assigner = {
+      type = "github";
+      owner = "NilFoundation";
+      repo = "evm-assigner";
+    };
+    nil_crypto3 = {
+      url = "https://github.com/NilFoundation/crypto3";
+      type = "git";
+      submodules = true;
+    };
+    # TODO: remove revision after merge of this PR in zkllvm-blueprint:
+    # https://github.com/NilFoundation/zkllvm-blueprint/pull/363
+    nil_zkllvm_blueprint = {
+      url = "https://github.com/NilFoundation/zkllvm-blueprint";
+      type = "git";
+      rev = "01964c21ab68e0cd77e08c0a9225f220a5b6bf2d";
+      submodules = true;
+    };
     intx = { url = "github:chfast/intx"; flake = false; };
     hashtree = { url = "github:prysmaticlabs/hashtree"; flake = false; };
     sszpp = { url = "github:OffchainLabs/sszpp"; flake = false; };
+    evmc = { url = "github:ethereum/evmc"; flake = false; };
   };
 
-  outputs = { self, nixpkgs, ... } @ repos:
+  outputs = { self, nixpkgs, nil_evm_assigner, nil_crypto3, nil_zkllvm_blueprint, ... } @ repos:
     let
       supportedSystems = [
         "x86_64-linux"
@@ -41,11 +61,19 @@
             repo = repos.sszpp;
             inherit pkgs lib enable_debug stdenv hashtree intx;
           };
+
+          evmc = (pkgs.callPackage ./nix/evmc.nix) {
+            repo = repos.evmc;
+            inherit pkgs lib stdenv enable_debug;
+          };
         };
 
       makeReleaseBuild = { pkgs }:
         let
           deps = allDependencies { enable_debug = false; inherit repos pkgs; };
+          evm_assigner = nil_evm_assigner.packages.${pkgs.system}.default;
+          crypto3 = nil_crypto3.packages.${pkgs.system}.default;
+          blueprint = nil_zkllvm_blueprint.packages.${pkgs.system}.default;
         in
         pkgs.gcc13Stdenv.mkDerivation {
           name = "zkEVM-framework";
@@ -53,7 +81,11 @@
           buildInputs = with pkgs; [
             cmake
             ninja
+            boost
             deps.sszpp
+            evm_assigner
+            crypto3
+            blueprint
           ];
 
           src = self;
@@ -64,6 +96,9 @@
       makeTests = { pkgs }:
         let
           deps = allDependencies { enable_debug = false; inherit repos pkgs; };
+          evm_assigner = nil_evm_assigner.packages.${pkgs.system}.default;
+          crypto3 = nil_crypto3.packages.${pkgs.system}.default;
+          blueprint = nil_zkllvm_blueprint.packages.${pkgs.system}.default;
         in
         pkgs.gcc13Stdenv.mkDerivation {
           name = "zkEVM-framework-tests";
@@ -73,7 +108,11 @@
           buildInputs = with pkgs; [
             cmake
             ninja
+            boost
             deps.sszpp
+            evm_assigner
+            crypto3
+            blueprint
           ];
 
           cmakeFlags = [ "-DENABLE_TESTS=TRUE" ];
@@ -91,15 +130,23 @@
       makeDevShell = { pkgs }:
         let
           deps = allDependencies { enable_debug = true; inherit repos pkgs; };
+          evm_assigner = nil_evm_assigner.packages.${pkgs.system}.default;
+          crypto3 = nil_crypto3.packages.${pkgs.system}.default;
+          blueprint = nil_zkllvm_blueprint.packages.${pkgs.system}.default;
+
         in
         pkgs.mkShell {
           buildInputs = with pkgs; [
             cmake
             ninja
+            boost
             gtest
             doxygen
             clang_17
             deps.sszpp
+            evm_assigner
+            crypto3
+            blueprint
           ];
 
           shellHook = ''
