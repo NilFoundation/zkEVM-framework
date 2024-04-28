c29bae458a93f5a9aac94a5a44467a1dcef89250
akokoshn <alexeikokoshnikof@nil.foundation>
2024-04-28 10:50:48 +0300

    Init account stoarage with simple contract

--- bin/assigner/include/utils.hpp
+++ bin/assigner/include/utils.hpp
@@ -6,176 +6,22 @@
 #include <map>
 
 #include "execution_state.hpp"
+#include "instructions_opcodes.hpp"
 #include "vm.hpp"
+#include "vm_host.h"
 #include "zkevm_framework/data_types/base.hpp"
 #include "zkevm_framework/data_types/block.hpp"
 
-struct vm_account {
-    virtual ~vm_account() = default;
-
-    // evmc::uint256be = evmc::bytes32 = uint8_t[32]
-
-    evmc::uint256be balance = {};
-    // code for execute call
-    std::vector<uint8_t> code;
-
-    // sload/sstore op codes
-    std::map<evmc::bytes32, evmc::bytes32> storage;
-    // tload/tstore op codes
-    std::map<evmc::bytes32, evmc::bytes32> transient_storage;
-
-    virtual evmc::bytes32 code_hash() const {
-        // Extremely dumb "hash" function.
-        evmc::bytes32 ret{};
-        for (const auto v : code) ret.bytes[v % sizeof(ret.bytes)] ^= v;
-        return ret;
-    }
-};
-
-using vm_accounts = std::map<evmc::address, vm_account>;
-
-class VmHost : public evmc::Host {
-    vm_accounts accounts;
-    evmc_tx_context tx_context{};
-
-  public:
-    VmHost() = default;
-    explicit VmHost(evmc_tx_context& _tx_context) noexcept : tx_context{_tx_context} {}
-    VmHost(evmc_tx_context& _tx_context, vm_accounts& _accounts) noexcept
-        : accounts{_accounts}, tx_context{_tx_context} {}
-
-    bool account_exists(const evmc::address& addr) const noexcept final {
-        return accounts.find(addr) != accounts.end();
-    }
-
-    evmc::bytes32 get_storage(const evmc::address& addr,
-                              const evmc::bytes32& key) const noexcept final {
-        const auto account_iter = accounts.find(addr);
-        if (account_iter == accounts.end()) {
-            return {};
-        }
-
-        const auto storage_iter = account_iter->second.storage.find(key);
-        if (storage_iter != account_iter->second.storage.end()) {
-            return storage_iter->second;
-        }
-        return {};
-    }
-
-    evmc_storage_status set_storage(const evmc::address& addr, const evmc::bytes32& key,
-                                    const evmc::bytes32& value) noexcept final {
-        if (accounts.find(addr) == accounts.end()) {
-            return EVMC_STORAGE_DELETED;
-        }
-        auto& account = accounts[addr];
-        auto prev_value = account.storage[key];
-        account.storage[key] = value;
-
-        return (prev_value == value) ? EVMC_STORAGE_ASSIGNED : EVMC_STORAGE_MODIFIED;
-    }
-
-    evmc::uint256be get_balance(const evmc::address& addr) const noexcept final {
-        auto it = accounts.find(addr);
-        if (it != accounts.end()) {
-            return it->second.balance;
-        }
-        return {};
-    }
-
-    size_t get_code_size(const evmc::address& addr) const noexcept final {
-        auto it = accounts.find(addr);
-        if (it != accounts.end()) {
-            return it->second.code.size();
-        }
-        return 0;
-    }
-
-    evmc::bytes32 get_code_hash(const evmc::address& addr) const noexcept final {
-        auto it = accounts.find(addr);
-        if (it != accounts.end()) {
-            return it->second.code_hash();
-        }
-        return {};
-    }
-
-    size_t copy_code(const evmc::address& addr, size_t code_offset, uint8_t* buffer_data,
-                     size_t buffer_size) const noexcept final {
-        const auto it = accounts.find(addr);
-        if (it == accounts.end()) {
-            return 0;
-        }
-
-        const auto& code = it->second.code;
-
-        if (code_offset >= code.size()) {
-            return 0;
-        }
-
-        const auto n = std::min(buffer_size, code.size() - code_offset);
-
-        if (n > 0) {
-            std::copy_n(&code[code_offset], n, buffer_data);
-        }
-        return n;
-    }
-
-    bool selfdestruct(const evmc::address& addr, const evmc::address& beneficiary) noexcept final {
-        (void)addr;
-        (void)beneficiary;
-        return false;
-    }
-
-    evmc::Result call(const evmc_message& msg) noexcept final {
-        return evmc::Result{EVMC_REVERT, msg.gas, 0, msg.input_data, msg.input_size};
-    }
-
-    evmc_tx_context get_tx_context() const noexcept final { return tx_context; }
-
-    evmc::bytes32 get_block_hash(int64_t number) const noexcept final {
-        evmc::bytes32 ret{};
-        return ret;
-    }
-
-    void emit_log(const evmc::address& addr, const uint8_t* data, size_t data_size,
-                  const evmc::bytes32 topics[], size_t topics_count) noexcept final {
-        (void)addr;
-        (void)data;
-        (void)data_size;
-        (void)topics;
-        (void)topics_count;
-    }
-
-    evmc_access_status access_account(const evmc::address& addr) noexcept final {
-        (void)addr;
-        return EVMC_ACCESS_COLD;
-    }
-
-    evmc_access_status access_storage(const evmc::address& addr,
-                                      const evmc::bytes32& key) noexcept final {
-        (void)addr;
-        (void)key;
-        return EVMC_ACCESS_COLD;
-    }
-
-    evmc::bytes32 get_transient_storage(const evmc::address& addr,
-                                        const evmc::bytes32& key) const noexcept override {
-        const auto account_iter = accounts.find(addr);
-        if (account_iter == accounts.end()) {
-            return {};
-        }
-
-        const auto transient_storage_iter = account_iter->second.transient_storage.find(key);
-        if (transient_storage_iter != account_iter->second.transient_storage.end()) {
-            return transient_storage_iter->second;
-        }
-        return {};
-    }
-
-    void set_transient_storage(const evmc::address& addr, const evmc::bytes32& key,
-                               const evmc::bytes32& value) noexcept override {
-        accounts[addr].transient_storage[key] = value;
-    }
-};
+evmc_call_kind evmc_msg_kind(const data_types::Transaction::Type& type) {
+    switch (type) {
+        case data_types::Transaction::Type::ContractCreation:
+            return EVMC_CREATE;
+        case data_types::Transaction::Type::MessageCall:
+            return EVMC_CALL;
+        default:
+            return EVMC_CALL;
+    };
+}
 
 template<typename T>
 evmc::uint256be to_uint256be(T v) {
