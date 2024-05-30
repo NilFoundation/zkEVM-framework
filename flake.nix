8cf01afadf7f06657a4a058e227c7babe3bf6517
Alexander Evgin <aleasims@gmail.com>
2024-05-30 20:15:00 +0400

    Refactor flake.nix
    This refactor includes a number of different changes just to make the code better
    and simplify extension process.

--- flake.nix
+++ flake.nix
@@ -2,24 +2,26 @@
   description = "Nix flake for zkEVM framework";
 
   inputs = {
-    nixpkgs = { url = "github:NixOS/nixpkgs"; };
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
     nix-3rdparty = {
       url = "github:NilFoundation/nix-3rdparty";
       inputs = {
         nixpkgs.follows = "nixpkgs";
+        flake-utils.follows = "flake-utils";
       };
     };
-    nil_evm_assigner = {
+    nil-evm-assigner = {
       type = "github";
       owner = "NilFoundation";
       repo = "evm-assigner";
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nil_crypto3.follows = "nil_crypto3";
-        nil_zkllvm_blueprint.follows = "nil_zkllvm_blueprint";
+        nil_crypto3.follows = "nil-crypto3";
+        nil_zkllvm_blueprint.follows = "nil-zkllvm-blueprint";
       };
     };
-    nil_crypto3 = {
+    nil-crypto3 = {
       url = "https://github.com/NilFoundation/crypto3";
       type = "git";
       submodules = true;
@@ -27,133 +29,130 @@
         nixpkgs.follows = "nixpkgs";
       };
     };
-    nil_zkllvm_blueprint = {
+    nil-zkllvm-blueprint = {
       url = "https://github.com/NilFoundation/zkllvm-blueprint";
       type = "git";
       submodules = true;
       inputs = {
         nixpkgs.follows = "nixpkgs";
-        nil_crypto3.follows = "nil_crypto3";
+        nil_crypto3.follows = "nil-crypto3";
       };
     };
   };
 
-  outputs = { self, nixpkgs, nix-3rdparty, nil_evm_assigner, nil_crypto3, nil_zkllvm_blueprint, ... } @ repos:
+  outputs =
+    { self
+    , nixpkgs
+    , flake-utils
+    , nix-3rdparty
+    , nil-evm-assigner
+    , nil-crypto3
+    , nil-zkllvm-blueprint
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
     let
-      supportedSystems = [
-        "x86_64-linux"
+      pkgs = import nixpkgs {
+        overlays = [ nix-3rdparty.overlays.${system}.default ];
+        inherit system;
+      };
+      evm_assigner = nil-evm-assigner.packages.${system}.default;
+      crypto3 = nil-crypto3.packages.${system}.default;
+      blueprint = nil-zkllvm-blueprint.packages.${system}.default;
+
+      # Default env will bring us GCC 13 as default compiler
+      stdenv = pkgs.stdenv;
+
+      defaultNativeBuildInputs = [
+        pkgs.cmake
+        pkgs.ninja
       ];
 
-      # For all supported systems call `f` providing system pkgs
-      forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs {
-          overlays = [ nix-3rdparty.overlays.${system}.default ];
-          inherit system;
-        };
+      defaultBuildInputs = [
+        # Default nixpkgs packages
+        pkgs.boost
+        pkgs.valijson
+        # Packages from nix-3rdparty
+        pkgs.sszpp
+        pkgs.evmc
+        # Repo dependencies
+        evm_assigner
+        crypto3
+        blueprint
+      ];
+
+      defaultCheckInputs = [
+        pkgs.gtest
+      ];
+
+      defaultDevTools = [
+        pkgs.doxygen
+        pkgs.clang_17 # clang-format and clang-tidy
+      ];
+
+      releaseBuild = stdenv.mkDerivation {
+        name = "zkEVM-framework";
+
+        nativeBuildInputs = defaultNativeBuildInputs;
+
+        buildInputs = defaultBuildInputs;
+
+        src = self;
+
+        cmakeBuildType = "Release";
+
+        doCheck = false;
+      };
+
+      # TODO: we need to propagate debug mode to dependencies here:
+      # Tracking issue: https://github.com/NilFoundation/zkEVM-framework/issues/58
+      debugBuild = releaseBuild.overrideAttrs (finalAttrs: previousAttrs: {
+        name = previousAttrs.name + "-debug";
+
+        cmakeBuildType = "Debug";
       });
 
-      makeReleaseBuild = { pkgs }:
-        let
-          evm_assigner = nil_evm_assigner.packages.${pkgs.system}.default;
-          crypto3 = nil_crypto3.packages.${pkgs.system}.default;
-          blueprint = nil_zkllvm_blueprint.packages.${pkgs.system}.default;
-        in
-        pkgs.gcc13Stdenv.mkDerivation {
-          name = "zkEVM-framework";
+      testBuild = releaseBuild.overrideAttrs (finalAttrs: previousAttrs: {
+        name = previousAttrs.name + "-tests";
 
-          buildInputs = with pkgs; [
-            cmake
-            ninja
-            boost
-            sszpp
-            evmc
-            valijson
-            evm_assigner
-            crypto3
-            blueprint
-          ];
+        cmakeFlags = [ "-DENABLE_TESTS=TRUE" ];
 
-          src = self;
+        doCheck = true;
 
-          doCheck = false;
-        };
+        checkInputs = defaultCheckInputs;
 
-      makeTests = { pkgs }:
-        let
-          evm_assigner = nil_evm_assigner.packages.${pkgs.system}.default;
-          crypto3 = nil_crypto3.packages.${pkgs.system}.default;
-          blueprint = nil_zkllvm_blueprint.packages.${pkgs.system}.default;
-        in
-        pkgs.gcc13Stdenv.mkDerivation {
-          name = "zkEVM-framework-tests";
+        GTEST_OUTPUT = "xml:${placeholder "out"}/test-reports/";
 
-          src = self;
+        dontInstall = true;
+      });
 
-          buildInputs = with pkgs; [
-            cmake
-            ninja
-            boost
-            sszpp
-            evmc
-            valijson
-            evm_assigner
-            crypto3
-            blueprint
-          ];
+      makeDevShell = pkgs.mkShell {
+        nativeBuildInputs = defaultNativeBuildInputs
+          ++ defaultBuildInputs
+          ++ defaultCheckInputs
+          ++ defaultDevTools;
 
-          cmakeFlags = [ "-DENABLE_TESTS=TRUE" ];
-
-          doCheck = true;
-          checkInputs = with pkgs; [
-            gtest
-          ];
-
-          GTEST_OUTPUT = "xml:${placeholder "out"}/test-reports/";
-
-          dontInstall = true;
-        };
-
-      makeDevShell = { pkgs }:
-        let
-          evm_assigner = nil_evm_assigner.packages.${pkgs.system}.default;
-          crypto3 = nil_crypto3.packages.${pkgs.system}.default;
-          blueprint = nil_zkllvm_blueprint.packages.${pkgs.system}.default;
-        in
-        pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            gcc13
-            cmake
-            ninja
-            boost
-            gtest
-            doxygen
-            clang_17
-            sszpp
-            evmc
-            valijson
-            evm_assigner
-            crypto3
-            blueprint
-          ];
-
-          shellHook = ''
-            echo "zkEVM dev environment activated"
-          '';
-        };
+        shellHook = ''
+          echo "zkEVM-framework dev environment activated"
+        '';
+      };
     in
     {
-      packages = forAllSystems ({ pkgs }: { default = makeReleaseBuild { inherit pkgs; }; });
-      checks = forAllSystems ({ pkgs }: { default = makeTests { inherit pkgs; }; });
-      apps = forAllSystems ({ pkgs }: {
+      packages = {
+        default = releaseBuild;
+        debug = debugBuild;
+      };
+      checks.default = testBuild;
+      apps = {
         assigner = {
-              type = "app";
-              program = "${self.packages.${pkgs.system}.default}/bin/assigner";
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/assigner";
         };
         block_gen = {
-              type = "app";
-              program = "${self.packages.${pkgs.system}.default}/bin/block_gen";
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/block_gen";
         };
-      });
-      devShells = forAllSystems ({ pkgs }: { default = makeDevShell { inherit pkgs; }; });
-    };
+      };
+      devShells.default = makeDevShell;
+    }
+    );
 }
