5466f3512f906c5a1fab03514b2c560171227377
Mikhail Aksenov <maksenov@nil.foundation>
2024-06-19 15:27:56 +0300

    [not working] Generate block to db, update nil

--- bin/block_generator/block_generator.go
+++ bin/block_generator/block_generator.go
@@ -1,135 +1,51 @@
-package block_generator
+package main
 
 import (
 	"context"
-	"log"
 	"strconv"
 
 	"github.com/NilFoundation/nil/common"
-	//"github.com/NilFoundation/nil/common/hexutil"
 	"github.com/NilFoundation/nil/core/db"
 	"github.com/NilFoundation/nil/core/execution"
 	"github.com/NilFoundation/nil/core/types"
-	"github.com/NilFoundation/nil/msgpool"
-	"github.com/NilFoundation/nil/rpc/transport"
-	"github.com/NilFoundation/nil/rpc/transport/rpccfg"
-	"github.com/NilFoundation/nil/rpc/jsonrpc"
 )
 
-/*
-func CreateBlock() Block {
-	suite.lastBlockHash = common.EmptyHash
-	for i := range types.BlockNumber(2) {
-		es, err := execution.NewExecutionState(tx, shardId, suite.lastBlockHash, common.NewTestTimer(0))
-		suite.Require().NoError(err)
-
-		for j := range int(i) {
-			m := &types.Message{Data: types.Code(strconv.Itoa(j))}
-			es.AddInMessage(m)
-
-			r := &types.Receipt{MsgHash: m.Hash()}
-			es.AddReceipt(r)
-		}
-
-		blockHash, err := es.Commit(i)
-		suite.Require().NoError(err)
-		suite.lastBlockHash = blockHash
-
-		block, err := execution.PostprocessBlock(tx, shardId, blockHash)
-		suite.Require().NotNil(block)
-		suite.Require().NoError(err)
+func CreateBlock(n types.BlockNumber) *types.Block {
+	badger, err := db.NewBadgerDb("test.db")
+	if err != nil {
+		return nil
+	}
+	tx, err := badger.CreateRwTx(context.Background())
+	if err != nil {
+		return nil
+	}
+	es, err := execution.NewExecutionState(tx, types.MasterShardId, common.EmptyHash, common.NewTestTimer(0))
+	if err != nil {
+		return nil
 	}
 
-	err = tx.Commit()
-	suite.Require().NoError(err)
+	m := &types.Message{Data: types.Code(strconv.Itoa(9))}
+	es.AddInMessage(m)
 
-	pool := msgpool.New(msgpool.DefaultConfig)
-	suite.Require().NotNil(pool)
+	r := &types.Receipt{MsgHash: m.Hash()}
+	es.AddReceipt(r)
 
-	suite.api = NewEthAPI(ctx,
-		NewBaseApi(rpccfg.DefaultEvmCallTimeout), suite.db, []msgpool.Pool{pool}, common.NewLogger("Test"))
+	blockHash, err := es.Commit(n)
+	if err != nil {
+		return nil
+	}
+
+	execution.PostprocessBlock(tx, types.MasterShardId, blockHash)
+	tx.Commit()
+	return db.ReadBlock(tx, types.MasterShardId, blockHash)
+	// badger.Close()
 }
-*/
 
 func main() {
-	//var conig_file_name = "config.jon"
-	shardId := types.MasterShardId
-	ctx := context.Background()
-
-	var _db db.DB
-	var err error
-	_db, err = db.NewBadgerDbInMemory()
-	if err != nil {
-		log.Println(err)
+	b := CreateBlock(1)
+	if b == nil {
+		println("Generation failed")
 		return
 	}
-
-	tx, err := _db.CreateRwTx(ctx)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	var lastBlockHash = common.EmptyHash
-
-	for i := range types.BlockNumber(2) {
-		es, err := execution.NewExecutionState(tx, shardId, lastBlockHash, common.NewTestTimer(0))
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		for j := range int(i) {
-			m := &types.Message{Data: types.Code(strconv.Itoa(j))}
-			es.AddInMessage(m)
-
-			r := &types.Receipt{MsgHash: m.Hash()}
-			es.AddReceipt(r)
-		}
-
-		blockHash, err := es.Commit(i)
-		if err != nil {
-			log.Println(err)
-			return
-		}
-
-		lastBlockHash = blockHash
-
-		block, err := execution.PostprocessBlock(tx, shardId, blockHash)
-		if block == nil {
-			log.Println("Empty block")
-			return
-		}
-		if err != nil {
-			log.Println(err)
-			return
-		}
-	}
-
-	err = tx.Commit()
-	if err != nil {
-		log.Println(err)
-		return
-	}
-
-	pool := msgpool.New(msgpool.DefaultConfig)
-	if pool == nil {
-		log.Println("Empty message pool")
-		return
-	}
-
-	api := jsonrpc.NewEthAPI(ctx,
-		jsonrpc.NewBaseApi(rpccfg.DefaultEvmCallTimeout), _db, []msgpool.Pool{pool}, common.NewLogger("Test"))
-	data, err := api.GetBlockByNumber(ctx, shardId, transport.BlockNumber(-5), false)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	if data == nil {
-		log.Println("can't get block by ID")
-		return
-	}
-	log.Println(data["parentHash"])
-
-	_db.Close()
+	println("Generation succeeded")
 }
--- bin/block_generator/go.mod
+++ bin/block_generator/go.mod
@@ -3,7 +3,8 @@ module block_generator
 go 1.22.3
 
 require (
-	github.com/NilFoundation/nil v0.0.0-20240530095817-3470e1f45e4a // indirect
+	github.com/NilFoundation/fastssz v0.1.4 // indirect
+	github.com/NilFoundation/nil v0.0.0-20240619092650-7d6cda35c4b6 // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.3.3 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
@@ -12,13 +13,14 @@ require (
 	github.com/dgraph-io/badger/v4 v4.2.0 // indirect
 	github.com/dgraph-io/ristretto v0.1.1 // indirect
 	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/ethereum/go-ethereum v1.14.3 // indirect
 	github.com/felixge/httpsnoop v1.0.3 // indirect
 	github.com/ferranbt/fastssz v0.1.4-0.20240422063434-a4db75388da1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/glog v1.2.1 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
-	github.com/golang/snappy v0.0.4 // indirect
+	github.com/golang/snappy v0.0.5-0.20220116011046-fa5810519dcb // indirect
 	github.com/google/btree v1.1.2 // indirect
 	github.com/google/flatbuffers v24.3.25+incompatible // indirect
 	github.com/gorilla/handlers v1.5.2 // indirect
@@ -44,8 +46,8 @@ require (
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/term v0.19.0 // indirect
-	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/protobuf v1.33.0 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
