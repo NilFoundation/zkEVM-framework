4322ee22d80ebe6f33d17080ad1b1004b0994bc5
Mikhail Aksenov <maksenov@nil.foundation>
2024-07-23 17:14:02 +0300

    Check is_satisfied for bytecode circuit

--- bin/assigner/include/checks.hpp
+++ bin/assigner/include/checks.hpp
@@ -11,13 +11,17 @@
 #include <nil/blueprint/blueprint/plonk/circuit.hpp>
 #include <nil/blueprint/utils/connectedness_check.hpp>
 #include <nil/blueprint/utils/satisfiability_check.hpp>
+#include <nil/blueprint/zkevm/bytecode.hpp>
+
+template<typename BlueprintFieldType>
+using ArithmetizationType = nil::crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType>;
 
 template<typename BlueprintFieldType>
 bool check_connectedness(
     const nil::blueprint::circuit<
-        nil::crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType>>& bp,
+        ArithmetizationType<BlueprintFieldType>>& bp,
     const nil::blueprint::assignment<
-        nil::crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType>>& assignment,
+        ArithmetizationType<BlueprintFieldType>>& assignment,
     const std::vector<
         nil::crypto3::zk::snark::plonk_variable<typename BlueprintFieldType::value_type>>&
         instance_input,
@@ -58,14 +62,46 @@ bool check_connectedness(
 template<typename BlueprintFieldType>
 bool is_satisfied(
     const nil::blueprint::circuit<
-        nil::crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType>>& bp,
+        ArithmetizationType<BlueprintFieldType>>& bp,
     const nil::blueprint::assignment<
-        nil::crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType>>& assignment) {
+        ArithmetizationType<BlueprintFieldType>>& assignment) {
     if (!nil::blueprint::is_satisfied(bp, assignment)) {
         std::cerr << "ERROR: is not satisfied\n";
         return false;
     }
     return true;
+}
+
+template<typename BlueprintFieldType>
+bool check_bytecode_satisfied(
+    nil::blueprint::assignment<ArithmetizationType<BlueprintFieldType>>& bytecode_table) {
+    using component_type =
+        nil::blueprint::components::zkevm_bytecode<ArithmetizationType<BlueprintFieldType>,
+                                                   BlueprintFieldType>;
+
+    // Prepare witness container to make an instance of the component
+    typename component_type::manifest_type m = component_type::get_manifest();
+    size_t witness_amount = *(m.witness_amount->begin());
+    std::vector<std::uint32_t> witnesses(witness_amount);
+    for (std::uint32_t i = 0; i < witness_amount; i++) {
+        witnesses[i] = i;
+    }
+
+    constexpr size_t max_code_size = 24576;
+    component_type component_instance = component_type(
+        witnesses, std::array<std::uint32_t, 1>{0}, std::array<std::uint32_t, 1>{0}, max_code_size);
+
+    nil::blueprint::circuit<nil::crypto3::zk::snark::plonk_constraint_system<BlueprintFieldType>>
+        bp;
+    auto lookup_tables = component_instance.component_lookup_tables();
+    for (auto& [k, v] : lookup_tables) {
+        bp.reserve_table(k);
+    }
+
+    // TODO: pass a proper public input here
+    typename component_type::input_type input({}, {}, typename component_type::var());
+    nil::blueprint::components::generate_circuit(component_instance, bp, bytecode_table, input, 0);
+    return ::is_satisfied(bp, bytecode_table);
 }
 
 #endif  // ZKEMV_FRAMEWORK_LIBS_CHECKS_INCLUDE_CHECKS_HPP_
